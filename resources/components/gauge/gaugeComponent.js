c4c039e1f18141f19a30d426680c87992d3a934d
Joel Latino <jlatino@sapo.pt>
2014-05-29 16:18:44 +0100

    Render grid implemented, #21 issue fixed

--- resources/components/gauge/gaugeComponent.js
+++ resources/components/gauge/gaugeComponent.js
@@ -1,5 +1,22 @@
 var gaugeComponent = BaseComponent.extend({
 
+	GUID: function(){
+		function S4() { return (((1+Math.random())*0x10000)|0).toString(16).substring(1); };
+		return (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
+	},
+
+	gaugeChartObj: [],
+
+	gridSize: function(rowNum){
+		if(rowNum == 1){
+			return [1, "col-md-12 span-24"];
+		}else if(rowNum > 3){
+			return [4, "col-md-3 span-6"];
+		}
+
+		return [2, "col-md-6 span-12"];
+	},
+
 	update : function() {
 		var myself=this;
 
@@ -15,83 +32,92 @@ var gaugeComponent = BaseComponent.extend({
 			var query = new Query(myself.queryDefinition);
 			
 			query.fetchData(myself.parameters, function(values) {
-				
-				console.log("Data:");
-				console.log(values);
-				
-				var actualValue, minValue, maxValue, title, subtitle;
-				
-				if(myself.label == undefined){
-					title = values.resultset[0][0];
-				} else {
-					title = myself.label;
-				}
-				
-				if(myself.subTitle == undefined){
-					subTitle = "";
-				} else {
-					subTitle = myself.subTitle;
-				}
-				
-				if(myself.maxValue == undefined){
-					minValue = values.resultset[0][2];
-				} else {
-					minValue = myself.minValue;
-				}
-				
-				if(myself.maxValue == undefined){
-					maxValue = values.resultset[0][3];
-				} else {
-					maxValue = myself.maxValue;
-				}
-				
-				var gageConf = {
-					id: myself.htmlObject, 
-					value: values.resultset[0][1], 
-					min: minValue,
-					max: maxValue,
-					title: title,
-					label:subTitle,
-					gaugeWidthScale:myself.gaugeWidthScale,
-          			symbol: myself.gaugeSymbol,
-        			relativeGaugeSize: true,
-        			formatNumber: myself.formatNumber,
-        			donut: myself.gaugeType == "doughnut"
-				};
 
-				if(myself.textRenderer != undefined && typeof myself.textRenderer == "function"){
-					gageConf.textRenderer = myself.textRenderer;
-				}
+				var gridConf = myself.gridSize(values.resultset.length);
 
-				if(values.resultset[0][4] !== undefined){
-					gageConf.gaugeColor = values.resultset[0][4];
-				}
-
-				if(values.resultset[0][5] !== undefined){
-					var lvlColors = [values.resultset[0][5]];
-					if(values.resultset[0][6] !== undefined){
-						lvlColors.push(values.resultset[0][6]);
+				var currRow, currCol;
+				for(var idx = 0; idx<values.resultset.length; idx++){
+					if(idx==0||idx%gridConf[0]==0){
+						currRow = myself.GUID();
+						$("#"+myself.htmlObject).append("<div class='row' id ="+currRow+"></div>");
 					}
-					if(values.resultset[0][7] !== undefined){
-						lvlColors.push(values.resultset[0][7]);
-					}
-					gageConf.levelColors = lvlColors;
-				}
 
-				var extension = {};
-				if(myself.extensionPoints != undefined && myself.extensionPoints[0] != undefined){
-					for(var i = 0; i < myself.extensionPoints.length; i++){
-						extension[myself.extensionPoints[i][0]] = typeof myself.extensionPoints[i][1] === 'function'?myself.extensionPoints[i][1]():myself.extensionPoints[i][1];
-					}
-				}
+					currCol = myself.GUID();
+					$("#"+currRow).append("<div class='"+gridConf[1]+"' id ="+currCol+"></div>");
 
-				gageConf = $.extend({}, gageConf, extension);
-				
-				myself.gaugeChartObj = new JustGage(gageConf);
-				if(myself.expression!==undefined){
-					$("#"+myself.htmlObject).click(function(e){
-						myself.expression(e);
-					});
+					var actualValue, minValue, maxValue, title, subtitle;
+					if(myself.label == undefined){
+						title = values.resultset[idx][0];
+					} else {
+						title = myself.label;
+					}
+					
+					if(myself.subTitle == undefined){
+						subTitle = "";
+					} else {
+						subTitle = myself.subTitle;
+					}
+					
+					if(myself.maxValue == undefined){
+						minValue = values.resultset[idx][2];
+					} else {
+						minValue = myself.minValue;
+					}
+					
+					if(myself.maxValue == undefined){
+						maxValue = values.resultset[idx][3];
+					} else {
+						maxValue = myself.maxValue;
+					}
+					
+					var gageConf = {
+						id: currCol, 
+						value: values.resultset[idx][1], 
+						min: minValue,
+						max: maxValue,
+						title: title,
+						label:subTitle,
+						gaugeWidthScale:myself.gaugeWidthScale,
+	          			symbol: myself.gaugeSymbol,
+	        			relativeGaugeSize: true,
+	        			formatNumber: myself.formatNumber,
+	        			donut: myself.gaugeType == "doughnut"
+					};
+
+					if(myself.textRenderer != undefined && typeof myself.textRenderer == "function"){
+						gageConf.textRenderer = myself.textRenderer;
+					}
+
+					if(values.resultset[idx][4] !== undefined){
+						gageConf.gaugeColor = values.resultset[idx][4];
+					}
+
+					if(values.resultset[idx][5] !== undefined){
+						var lvlColors = [values.resultset[idx][5]];
+						if(values.resultset[idx][6] !== undefined){
+							lvlColors.push(values.resultset[idx][6]);
+						}
+						if(values.resultset[idx][7] !== undefined){
+							lvlColors.push(values.resultset[idx][7]);
+						}
+						gageConf.levelColors = lvlColors;
+					}
+
+					var extension = {};
+					if(myself.extensionPoints != undefined && myself.extensionPoints[0] != undefined){
+						for(var i = 0; i < myself.extensionPoints.length; i++){
+							extension[myself.extensionPoints[i][0]] = typeof myself.extensionPoints[i][1] === 'function'?myself.extensionPoints[i][1]():myself.extensionPoints[i][1];
+						}
+					}
+
+					gageConf = $.extend({}, gageConf, extension);
+					
+					myself.gaugeChartObj[idx] = new JustGage(gageConf);
+					if(myself.expression!==undefined){
+						$("#"+currCol).click(function(e){
+							myself.expression(e);
+						});
+					}
 				}
 			});
 		}
