eba19ef87f377aec618687da13f1c9a09b62f17f
Joel Latino <jlatino@sapo.pt>
2014-07-12 20:15:02 +0100

    Add license header

--- resources/components/jQVectorMapsComponent/jQVectorMapsImplementation.js
+++ resources/components/jQVectorMapsComponent/jQVectorMapsImplementation.js
@@ -1,3 +1,15 @@
+/*!
+* Copyright 2014 - 2014 Ivy Information Systems Ltd.  All rights reserved.
+* 
+* This software was developed by Webdetails and is provided under the terms
+* of the Mozilla Public License, Version 2.0, or any later version. You may not use
+* this file except in compliance with the license. If you need a copy of the license,
+* please go to  http://mozilla.org/MPL/2.0/. The Initial Developer is Webdetails.
+*
+* Software distributed under the Mozilla Public License is distributed on an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to
+* the license for the specific language governing your rights and limitations.
+*/
 var jQVectorMapComponent = BaseComponent.extend({
 	update : function() {
 		if(this.vectorMap){
